e102c20adf13c759f5f77bff2e76b8266facd301
mrleonardobrito <lbritogit@gmail.com>
2025-06-06 18:47:04 -0300

    separating interfaces

--- repo/interface.go
+++ repo/interface.go
@@ -5,19 +5,19 @@ import (
 	"time"
 )
 
-type TechMarketRepository interface {
-	// Insert Operations
-	BatchCreateClient(clients []model.Client) error
-	BatchCreateProduct(products []model.Product) error
-	BatchCreateOrder(orders []model.Order) error
-	BatchCreateOrderItem(orderItems []model.OrderItem) error
-	BatchCreatePayment(payments []model.Payment) error
-
-	// Query Operations
+type QueryRepository interface {
 	GetClientByEmail(email string) (model.Client, error)
 	GetProductByCategory(category string) ([]model.Product, error)
 	GetDeliveredProductsByClient(clientID uint) ([]model.Product, error)
 	Get5MostSoldProducts() ([]model.Product, error)
 	GetLastMonthPixPayments() ([]model.Payment, error)
 	GetClientTotalSpentByPeriod(clientID uint, startDate time.Time, endDate time.Time) (float64, error)
+}
+
+type InsertRepository interface {
+	BatchCreateClient(clients []model.Client) error
+	BatchCreateProduct(products []model.Product) error
+	BatchCreateOrder(orders []model.Order) error
+	BatchCreateOrderItem(orderItems []model.OrderItem) error
+	BatchCreatePayment(payments []model.Payment) error
 }
